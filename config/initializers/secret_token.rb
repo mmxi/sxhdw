c0f4231350b88e5d06091332790f4d094fe872a7
Steven Wang <cjdx4311@gmail.com>
2011-08-01 18:40:03 +0000

    initial revision

---
+++ config/initializers/secret_token.rb
@@ -0,0 +1,7 @@
+# Be sure to restart your server when you modify this file.
+
+# Your secret key for verifying the integrity of signed cookies.
+# If you change this key, all old signed cookies will become invalid!
+# Make sure the secret is at least 30 characters and all random,
+# no regular words or you'll be exposed to dictionary attacks.
+Sxhdw::Application.config.secret_token = '6281a6e48f64ed4597d4dc9d66cb10442a95c4a09adf8ba9e996d4f24ed515263674b761efa466a97487cad1982f34d2db23477cda943bfaf655e669f6ff2683'
