bc9b421170c12f1a28351c803e334cc36acf5948
Steven Wang <cjdx4311@gmail.com>
2011-09-23 13:10:56 +0000

    remove generate_user_bindid function and move get_oauth_user_image function to application_helper.rb

--- app/helpers/application_helper.rb
+++ app/helpers/application_helper.rb
@@ -1,2 +1,14 @@
 module ApplicationHelper
+  def get_oauth_user_image(oauth)
+    provider = oauth["provider"]
+    user_image = "" 
+    if provider == "qzone"
+      user_image = oauth["user_info"]["urls"]["figureurl_1"]
+    elsif provider == "tqq"
+      user_image = oauth["user_info"]["image"] + "/50"
+    else
+      user_image = oauth["user_info"]["image"]
+    end
+    user_image
+  end
 end
