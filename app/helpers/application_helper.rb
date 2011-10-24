bef774bff8822729a7608845f71cfc34d1d8a3cb
Steven Wang <cjdx4311@gmail.com>
2011-10-24 11:19:13 +0800

    add ajax login

--- app/helpers/application_helper.rb
+++ app/helpers/application_helper.rb
@@ -1,2 +1,11 @@
 module ApplicationHelper
+  def pageless(total_pages, url=nil, container=nil)
+    opts = {
+      :totalPages => total_pages,
+      :url => url,
+    }
+
+    container && opts[:container] ||= container
+    javascript_tag("$('#topics').pageless(#{opts.to_json});")
+  end
 end
