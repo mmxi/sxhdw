1ea3dd4fc208943cf3ea6503c22dc62dd04a1602
Steven Wang <cjdx4311@gmail.com>
2011-10-30 22:51:38 +0800

    change rails_kindeditor directory

--- public/javascripts/application.js
+++ public/javascripts/application.js
@@ -1,7 +1,5 @@
-// Place your application-specific JavaScript functions and classes here
-// This file is automatically included by javascript_include_tag :defaults
+jQuery.ajaxSetup({ 'beforeSend': function(xhr){xhr.setRequestHeader("Accept", "text/javascript")}});
 
-//setTimeout(function() {;$$(".popup").each(function(value, index) {value.hide();});}, 3000);
 jQuery.fn.topLink = function(settings) {
   settings = jQuery.extend({
     min: 1,
@@ -23,8 +21,6 @@ jQuery.fn.topLink = function(settings) {
   });
 };
 
-jQuery(function(){$.datepicker.regional['zh-CN']={clearText:'清除',clearStatus:'清除已选日期',closeText:'关闭',closeStatus:'不改变当前选择',prevText:'<上月',prevStatus:'显示上月',prevBigText:'<<',prevBigStatus:'显示上一年',nextText:'下月>',nextStatus:'显示下月',nextBigText:'>>',nextBigStatus:'显示下一年',currentText:'今天',currentStatus:'显示本月',monthNames:['一月','二月','三月','四月','五月','六月','七月','八月','九月','十月','十一月','十二月'],monthNamesShort:['一','二','三','四','五','六','七','八','九','十','十一','十二'],monthStatus:'选择月份',yearStatus:'选择年份',weekHeader:'周',weekStatus:'年内周次',dayNames:['星期日','星期一','星期二','星期三','星期四','星期五','星期六'],dayNamesShort:['周日','周一','周二','周三','周四','周五','周六'],dayNamesMin:['日','一','二','三','四','五','六'],dayStatus:'设置 DD 为一周起始',dateStatus:'选择 m月 d日, DD',dateFormat:'yy-mm-dd',firstDay:1,initStatus:'请选择日期',isRTL:false};$.datepicker.setDefaults($.datepicker.regional['zh-CN']);});
-jQuery.ajaxSetup({ 'beforeSend': function(xhr){xhr.setRequestHeader("Accept", "text/javascript")}})
 jQuery.fn.extend({
   insertAtCaret: function(myValue){
   var obj;
@@ -51,8 +47,7 @@ jQuery.fn.extend({
     obj.focus();
    }
  }
-})
-
+});
 
 function init_dropdownmenu(){
   $("ul.dropdown li").hover(function(){
@@ -73,21 +68,22 @@ function forumhover() {
     $(this).removeClass('hover');
   });
 }
+
+function topichhover() {
+  $('.topic').hover(function(){
+    $(this).addClass('hover');
+  }, function(){
+    $(this).removeClass('hover');
+  });
+}
+
 function popupCenter(url, width, height, name) {
 	var left = (screen.width/2)-(width/2);
 	var top = (screen.height/2)-(height/2);
 	window.open(url, name, "menubar=no,toolbar=no,status=no,width="+width+",height="+height+",toolbar=no,left="+left+",top="+top);
 }
 
-jQuery(document).ready(function(){
-	jQuery(".popup").click(function(){
-		popupCenter(jQuery(this).attr("href"), jQuery(this).attr("data-width"), jQuery(this).attr("data-height"));
-		return false;
-	});
-	init_dropdownmenu(); // init dropdown menu
-	jQuery("#activity_start_time").datetimepicker();
-	jQuery("#activity_end_time").datetimepicker();
-  
+function ajaxbind(){
   $(document).bind('ajaxSend', function(e, request, options) {
     if ('POST' == options.type) {
       $('.floading').show();
@@ -95,15 +91,28 @@ jQuery(document).ready(function(){
       $('#loading').show();
     }
   });
+
   $(document).bind('ajaxComplete', function(e, request, options) {
     $('#loading').hide();
     $('.floading').hide();
   });
+}
+
+jQuery(document).ready(function(){
+  ajaxbind();
+  forumhover();
+  topichhover();
+  init_dropdownmenu();
+
+
 
   $('#top-link').topLink({min: 400, fadeSpeed: 500});
   $('#top-link').click(function(e) {e.preventDefault(); $(document).scrollTop(0);});
 
-  forumhover();
+	jQuery(".popup").click(function(){
+		popupCenter(jQuery(this).attr("href"), jQuery(this).attr("data-width"), jQuery(this).attr("data-height"));
+		return false;
+	});
 });
 
 
